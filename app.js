29cb03aa7028755ddfcf29bc2b3ee8b4b42e529f
alibarmonster <alibar838@gmail.com>
2023-04-01 07:03:05 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`app listen on ${PORT}`);
+});
